6be19e88bfaad4ea72989c57f21d2459c38c9ef3
Kenan Rustamov <kenanrustamov@gmail.com>
2019-09-11 13:31:44 -0400

    Updates

---
+++ precache-manifest.3bd0062b75282b988a4a3b9810ab3c71.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c250ee5831ebc009e5e31adb3ad78ab5",
+    "url": "/Partify/index.html"
+  },
+  {
+    "revision": "e935afa5a570e00c1374",
+    "url": "/Partify/static/css/main.0744b407.chunk.css"
+  },
+  {
+    "revision": "bb6a1b9acfd3676ae49b",
+    "url": "/Partify/static/js/2.7edd7b4c.chunk.js"
+  },
+  {
+    "revision": "e935afa5a570e00c1374",
+    "url": "/Partify/static/js/main.1142b566.chunk.js"
+  },
+  {
+    "revision": "c666fbb95aa7089099f2",
+    "url": "/Partify/static/js/runtime~main.4d582ee0.js"
+  },
+  {
+    "revision": "123a62b650944663a415a90d2e699590",
+    "url": "/Partify/static/media/background.123a62b6.jpg"
+  },
+  {
+    "revision": "2eab274f81868abdd942651c73716978",
+    "url": "/Partify/static/media/elijahKajinic.2eab274f.jpeg"
+  },
+  {
+    "revision": "31e405864f3048086e24e737080267f6",
+    "url": "/Partify/static/media/kenanRustamov.31e40586.jpg"
+  }
+]);
