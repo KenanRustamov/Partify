ecdffe7944c5b3b6bdc3140b72b874a6695a00e8
Kenan Rustamov <kenanrustamov@gmail.com>
2019-09-12 23:35:08 -0400

    Updates

---
+++ precache-manifest.b5f10671a5c2914753053ce625563f9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34068d404a579385d79d942c02a90ae8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf5ccd87ea191b619114",
+    "url": "/static/css/main.8be68213.chunk.css"
+  },
+  {
+    "revision": "bb6a1b9acfd3676ae49b",
+    "url": "/static/js/2.7edd7b4c.chunk.js"
+  },
+  {
+    "revision": "cf5ccd87ea191b619114",
+    "url": "/static/js/main.425a992a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "123a62b650944663a415a90d2e699590",
+    "url": "/static/media/background.123a62b6.jpg"
+  },
+  {
+    "revision": "2eab274f81868abdd942651c73716978",
+    "url": "/static/media/elijahKajinic.2eab274f.jpeg"
+  },
+  {
+    "revision": "31e405864f3048086e24e737080267f6",
+    "url": "/static/media/kenanRustamov.31e40586.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Partify/precache-manifest.6588e227ef9b63e451c2cb9feb71d62e.js"
+  "/precache-manifest.b5f10671a5c2914753053ce625563f9e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Partify/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
